c506662e3fbc28ccd319f7f24bfea385e922f07c
neuralduck <135601034+neuralduck@users.noreply.github.com>
2024-08-20 18:42:58 +0530

    removed console logs

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 var count = 0;
 var indices = []
-//console.log(localStorage)
 
-//console.log(localStorage.getItem("indices"));
-//console.log(localStorage.getItem("indices") !== null);
-//console.log(localStorage.getItem("indices") == '');
 if((localStorage.getItem("indices") !== null)&&(localStorage.getItem("indices") !== '')){
     indices = localStorage.getItem("indices").split(",");
     count = indices.length-1;
-    //console.log(indices);
     for(let index of indices){
-        //console.log("index: ", index);
-       // console.log(localStorage.getItem(index))
         var node = document.createElement("div");
         node.id = index;
         text = localStorage.getItem(index)
@@ -19,9 +12,8 @@ if((localStorage.getItem("indices") !== null)&&(localStorage.getItem("indices") 
         document.body.appendChild(node);
     }
 }
-//localStorage.clear();
+
 function add_todo() {
-    //alert(document.getElementById('entry').value);
     const text = document.getElementById("input").value;
     document.getElementById("input").value = "";
     if (text !== "") {
@@ -36,9 +28,8 @@ function add_todo() {
     }
     
 }
+
 function remove(element){
-    //console.log('in the delete block');
-    //console.log(element.parentNode.id);
     localStorage.removeItem(element.parentNode.id);
     indices.splice(indices.indexOf(element.parentNode.id), 1);
     localStorage.setItem("indices", indices);
